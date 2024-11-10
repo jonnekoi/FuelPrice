1326321a2847538845a0f8256cd7d40d7a064ac4
jonnek <jonnekoivisto00@gmail.com>
2024-11-10 18:45:40 +0200

    now localization works, also saves data to database

--- src/main/java/org/example/fuelprice/HelloApplication.java
+++ src/main/java/org/example/fuelprice/HelloApplication.java
@@ -5,11 +5,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.ResourceBundle;
+
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
+        fxmlLoader.setResources(ResourceBundle.getBundle("bundle1"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 640);
         stage.setTitle("Jonne Koivisto");
         stage.setScene(scene);
--- src/main/java/org/example/fuelprice/HelloController.java
+++ src/main/java/org/example/fuelprice/HelloController.java
@@ -2,10 +2,20 @@ package org.example.fuelprice;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
@@ -26,55 +36,88 @@ public class HelloController {
     private Text polttoaineenHintaText;
 
     @FXML
-    private Button enButton;
-
-    @FXML
     private Button fiButton;
 
-    @FXML
-    private Button jpButton;
+    private String language = "fi";
 
-    @FXML
-    private Button faButton;
-
-    private ResourceBundle bundle;
-
-    private Locale locale;
-
-    // private static final double fuelPrice = 1.7;
-
+    private static final String url = "jdbc:mysql://localhost:3306/fuelprice";
+    private static final String user = "root";
+    private static final String password = "";
 
     @FXML
     protected void calculatePrice() {
-        double fuelPrice = Double.parseDouble(priceField.getText().replace(",", "."));
-        double distance = Double.parseDouble(distanceField.getText().replace(",", "."));
-        double consumption = Double.parseDouble(kulutusField.getText().replace(",", "."));
-        double price = distance * (consumption / 100) * fuelPrice;
-        polttoaineenHintaText.setText("Fuel price: " + fuelPrice + "€/l");
-        resultText.setText("The price is: " + price + "€");
+        try {
+            double fuelPrice = Double.parseDouble(priceField.getText().replace(",", "."));
+            double distance = Double.parseDouble(distanceField.getText().replace(",", "."));
+            double consumption = Double.parseDouble(kulutusField.getText().replace(",", "."));
+            double price = distance * (consumption / 100) * fuelPrice;
+            polttoaineenHintaText.setText(fuelPrice + "€/l");
+            resultText.setText(price + "€");
+            saveData();
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
     }
 
-    @FXML
-    public void loadLanguage(String lang) {
-        Locale locale = new Locale(lang);
-        bundle = ResourceBundle.getBundle("language", locale);
-
+    public void setIR(ActionEvent actionEvent) {
+        Locale l = new Locale("ir");
+        language = "ir";
+        loadView(l);
     }
 
-    @FXML
-    public void fiButtonClick(ActionEvent actionEvent) {
-        loadLanguage("fi");
+    public void setJP(ActionEvent actionEvent) {
+        Locale l = new Locale("jp");
+        language = "jp";
+        loadView(l);
     }
-    @FXML
-    public void faButtonClick(ActionEvent actionEvent) {
-        loadLanguage("fa");
+
+    public void setFI(ActionEvent actionEvent) {
+        Locale l = new Locale("fi");
+        language = "fi";
+        loadView(l);
     }
-    @FXML
-    public void jpButtonClick(ActionEvent actionEvent) {
-        loadLanguage("jp");
+
+    public void setEN(ActionEvent actionEvent) {
+        Locale l = new Locale("en");
+        language = "en";
+        loadView(l);
     }
-    @FXML
-    public void enButtonClick(ActionEvent actionEvent) {
-        loadLanguage("en");
+
+    public void loadView(Locale locale) {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        fxmlLoader.setResources(ResourceBundle.getBundle("bundle", locale));
+        try {
+            Parent root = fxmlLoader.load();
+            Stage stage = (Stage) fiButton.getScene().getWindow();
+            Scene currentScene = stage.getScene();
+            Scene newScene = new Scene(root, currentScene.getWidth(), currentScene.getHeight());
+            stage.setScene(newScene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void saveData() {
+        int fuel_price = Integer.parseInt(priceField.getText());
+        int distance = Integer.parseInt(distanceField.getText());
+        int consumption = Integer.parseInt(kulutusField.getText());
+        String language_used = language;
+
+        String tablename = "data";
+
+        String sql = "INSERT INTO " + tablename + " (fuel_price, distance, consumption, language_used) VALUES (?, ?, ?, ?)";
+
+        try (Connection conn = DriverManager.getConnection(url, user, password);
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, String.valueOf(fuel_price));
+            pstmt.setString(2, String.valueOf(distance));
+            pstmt.setString(3, String.valueOf(consumption));
+            pstmt.setString(4, language_used);
+
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
